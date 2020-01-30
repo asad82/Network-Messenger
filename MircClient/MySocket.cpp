a770a0ca72dea25ac2381d8d021f96700c5d00de
Asad Ali <asad82@users.noreply.github.com>
2020-01-30 21:03:03 +0500

    Update MySocket.cpp

--- MircClient/MySocket.cpp
+++ MircClient/MySocket.cpp
@@ -1,5 +1,11 @@
 // MySocket.cpp : implementation file
 //
+/***************************************************************************
+copyright            : (C) Asad Ali
+email                : asad_82@yahoo.com
+website		     : sites.google.com/site/asad82
+***************************************************************************/
+// Programmed by Asad Ali
 
 #include "stdafx.h"
 #include "Client.h"
